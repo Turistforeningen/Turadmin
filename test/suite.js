4d8a10e69d6e86de8d71d54e34bea7b8c1f8ffde
Torbjørn Meistad <torbjorn.meistad@bouvet.no>
2014-01-17 09:50:08 +0100

    Formated to satisfy jslint and jshint

--- test/suite.js
+++ test/suite.js
@@ -4,17 +4,19 @@ var request = require('supertest');
 var app = null;
 var req = null;
 
-before(function() {
-  app = require('../app.js');
-  req = request(app);
+before(function () {
+    "use strict";
+    app = require('../app.js');
+    req = request(app);
 });
 
-describe('/', function() {
-  it('should be able to start the server without crashing', function(done) {
-    req.get('/').expect(200).end(function(err, res) {
-      assert.ifError(err);
-      done()
+describe('/', function () {
+    "use strict";
+    it('should be able to start the server without crashing', function (done) {
+        req.get('/').expect(200).end(function (err, res) {
+            assert.ifError(err);
+            done();
+        });
     });
-  });
 });
 
