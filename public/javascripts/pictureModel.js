97b0c158d18616e2ff09abf5fbed8693aaea97ca
Torbjørn Meistad <torbjorn.meistad@bouvet.no>
2014-01-22 09:51:42 +0100

    Models for poi and picture

---
+++ public/javascripts/pictureModel.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) $year, Den Norske Turistforening (DNT)
+ *
+ * https://github.com/Turistforeningen/turadmin
+ */
+
+var DNT = window.DNT || {};
+
+(function (ns) {
+    "use strict";
+
+    var apiUri = function () {
+        return "/apiProxy/bilder";
+    };
+
+    ns.Picture = Backbone.Model.extend({
+        idAttribute: "_id",
+
+        urlRoot: function () {
+            return apiUri();
+        },
+
+        defaults : {
+            geojson: null,
+            lisens: "CC BY 3.0 NO",
+            status: "Kladd",
+            privat: {
+                opprettet_av: {
+                    id: "someId"
+                }
+            }
+        },
+        initialize: function () {
+        }
+    });
+
+    ns.PictureList = Backbone.Collection.extend({
+        url: function () {
+            return apiUri();
+        },
+        model: ns.Picture
+    });
+}(DNT));
+
