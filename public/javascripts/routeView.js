4e60e0f543b27e1624f62a57e4dae6a6af4c0f0a
Torbjørn Meistad <torbjorn.meistad@bouvet.no>
2014-01-29 11:38:50 +0100

    Created picturesView.js and moved file upload js code

--- public/javascripts/routeView.js
+++ public/javascripts/routeView.js
@@ -15,6 +15,7 @@ var DNT = window.DNT || {};
 
         initialize : function () {
             this.mapView = new DNT.MapView({model: this.model});
+            this.pictureView = new DNT.PicturesView({model: this.model});
             this.route = this.model.get("route");
             this.route.on("change", this.unsavedChanges, this);
             this.poiCollection = this.model.get("poiCollection");
