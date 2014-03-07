71feba2f727feec3d2f647c573439b4a08536c7b
Håvard Ranum <havard.ranum@bouvet.no>
2014-03-07 16:06:25 +0100

    Now pushing pictures to route view when editing

--- routes/route.js
+++ routes/route.js
@@ -8,6 +8,8 @@
 module.exports = function (app, restProxy, options) {
     "use strict";
 
+    var underscore = require('underscore');
+
     /*
      * GET add new route page.
      */
@@ -20,17 +22,51 @@ module.exports = function (app, restProxy, options) {
 
         var turId = req.params.id;
 
+        req.session.userId = "testUserId";
+
         // TODO: Fix dynamic URL
         var url = 'http://localhost:3000/restProxy/turer/' + turId;
 
         var onCompleteTurRequest = function (data) {
-            req.session.userId = "testUserId";
 
-            res.render('route', {
-                pageTitle: data.navn,
-                routeApiUri: options.routeApiUri,
-                turData: data
+            var picturesCount = data.bilder.length;
+            var picturesData = [];
+
+            // var poisCount = data.steder.length;
+            // var poisData = [];
+
+            var additionalResourcesCount = picturesCount; // TODO: Add poisCount
+
+            var onCompletePictureRequest = function (data) {
+                picturesData.push(data);
+                doRender();
+            };
+
+            // var onCompletePoiRequest = function (data) {
+            //     poisData.push(data);
+            //     doRender();
+            // };
+
+            for (var i = 0; i < picturesCount; i++) {
+                var pictureId = data.bilder[i];
+                restProxy.makeApiRequest('/bilder/' + pictureId, 'GET', undefined, onCompletePictureRequest);
+            }
+
+            // for (var i = 0; i < poisCount; i++) {
+            //     var poiId = data.steder[i];
+            //     restProxy.makeApiRequest("/steder/" + poiId, 'GET', undefined, doRender);
+            // }
+
+            var doRender = underscore.after(additionalResourcesCount, function(){
+                res.render('route', {
+                    pageTitle: data.navn,
+                    routeApiUri: options.routeApiUri,
+                    turData: data,
+                    picturesData: picturesData
+                    // poisData: poisData
+                });
             });
+
         };
 
         restProxy.makeApiRequest("/turer/" + turId, 'GET', undefined, onCompleteTurRequest);
