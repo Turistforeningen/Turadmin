a2e2d40c60308104780e1f576fd6be365f463fe8
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2014-04-02 08:53:58 +0200

    Add initial route provider for DNT Connect signon

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@
  * GET home page.
  */
 
-
 module.exports = function (app, options) {
     "use strict";
+
+    var connect = options.connect;
 
     /*
         GET list of routes (index page)
@@ -16,10 +17,31 @@ module.exports = function (app, options) {
         res.render('index', { title: 'Mine turer' });
     };
 
+    var getConnect = function (req, res) {
+      // Check for ?data= query
+      if (req && req.query && req.query.data) {
+        try {
+          var data = client.decryptJSON(req.query.data);
+        } catch (e) {
+          // @TODO handle this error propperly
+          var data = {er_autentisert: false}
+        }
+
+        if (data.er_autentisert === true) {
+          // User is authenticated
+        } else {
+          // User is not authenticated
+        }
+
+      // Initiate DNT Connect signon
+      } else {
+        res.redirect(connect.signon('http://localhost:3004/connect'));
+      }
+    };
+
     app.get('/', getIndex);
     app.get('/index', getIndex);
+    app.get('/connect', getConnect);
 
 };
-
-
 
