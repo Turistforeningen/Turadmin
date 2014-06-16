0de17694a44101662bb3f069f9bd2b5e802218eb
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2014-06-16 17:14:29 +0200

    Refactor with jfum and s3-uploader

--- routes/pictureUpload.js
+++ routes/pictureUpload.js
@@ -7,45 +7,23 @@
 module.exports = function (router) { // TODO: Pass router instead of app as argument in app.js
     "use strict";
 
-    var gm = require('gm').subClass({imageMagick: true});
+    var async = require('async');
+    var JFUM = require('jfum');
+    var Upload = require('s3-uploader');
     var dms2dec = require('dms2dec');
-    var path = require('path');
-    var fs = require('fs');
-    var _ = require('underscore');
 
     var ntbApi = require('./restProxy')(router, {ntbApiUri: process.env.NTB_API_URL, ntbApiKey: process.env.NTB_API_KEY});
-
-    var options = {
-        tmpDir: '/tmp', // tmp dir to upload files to
-        uploadDir: '/tmp', // actual location of the file
-        uploadUrl: '/uploads/', // end point for delete route
-        maxPostSize: 11000000000, // 11 GB
-        minFileSize: 1,
-        maxFileSize: 10000000000, // 10 GB
-        acceptFileTypes: /.+/i,
-        inlineFileTypes: /\.(gif|jpe?g|png)/i,
-        imageTypes:  /\.(gif|jpe?g|png)/i,
-        accessControl: {
-            allowOrigin: '*',
-            allowMethods: 'OPTIONS, HEAD, GET, POST, PUT, DELETE',
-            allowHeaders: 'Content-Type, Content-Range, Content-Disposition'
-        },
-        storage : {
-            type : 'local' // local or aws
-        }
-    };
-
-    var uploader = require('blueimp-file-upload-expressjs')(options);
-
-    var s3Uploader = require('s3-uploader');
-
+    var jfum = new JFUM();
     var s3Client = new Upload('turadmin', {
         awsBucketUrl: 'https://s3-eu-west-1.amazonaws.com/turadmin/',
-        awsBucketPath: process.env.AWS_BUCKET_PATH + '/',
+        awsBucketPath: process.env.AWS_BUCKET_PATH,
         awsBucketAcl: 'public-read',
+        returnExif: true,
+
         versions: [
             {
-                original: true
+                original: true,
+                awsImageAcl: 'private'
             },
             {
                 maxHeight: 1040,
@@ -66,62 +44,25 @@ module.exports = function (router) { // TODO: Pass router instead of app as argu
         ]
     });
 
+    var getImagePosition = function (exif) {
+        var lat = exif['exif:GPSLatitude'],
+            latRef = exif['exif:GPSLatitudeRef'],
+            lon = exif['exif:GPSLongitude'],
+            lonRef = exif['exif:GPSLongitudeRef'];
 
-    // Return image path based on file upload module options and image name
-    var getImagePath = function (image) {
-        return path.join(options.uploadDir, image.name);
-    };
+        if (!!lat && !!latRef && !!lon && !!lonRef) {
+            var dec = dms2dec(lat, latRef, lon, lonRef),
+                decLat = dec[0],
+                decLon = dec[1];
 
-    // Returns an image object with `geojson` property appended if image has gps info
-    var getImagePosition = function (image, callback) {
-
-        var imagePath = getImagePath(image);
-
-        gm(imagePath).identify(function (err, value) {
-
-            if (value['Properties']) {
-                var lat = value['Properties']['exif:GPSLatitude'];
-                var latRef = value['Properties']['exif:GPSLatitudeRef'];
-                var lon = value['Properties']['exif:GPSLongitude'];
-                var lonRef = value['Properties']['exif:GPSLongitudeRef'];
-
-                if (!!lat && !!latRef && !!lon && !!lonRef) {
-                    var dec = dms2dec(lat, latRef, lon, lonRef),
-                        decLat = dec[0],
-                        decLon = dec[1];
-
-                    image.geojson = {
-                        type: 'Point',
-                        coordinates: [decLon, decLat]
-                    };
-
-                }
-            }
-
-            callback(err, image);
-
-        });
-
-    };
-
-    var uploadImageToS3 = function (image, callback) {
-
-        var imagePath = getImagePath(image);
-
-        s3Client.upload(imagePath, function (err, images, exifData) {
-            if (err) {
-                console.error('Error when uploading picture to S3', err);
-                callback(err, images);
-            } else {
-                fs.unlink(imagePath);
-                callback(err, images);
-            }
-        });
-
+            return {
+                type: 'Point',
+                coordinates: [decLon, decLat]
+            };
+        }
     };
 
     var saveToNasjonalturbase = function (picture, callback) {
-
         // Need a request object for the ntbApi proxy
         var request = {
             method: 'POST',
@@ -129,7 +70,8 @@ module.exports = function (router) { // TODO: Pass router instead of app as argu
         };
 
         ntbApi.makeApiRequest('/bilder', request, undefined, function (data, result) {
-            var err = (result instanceof Error === true) ? result : null; // https://github.com/danwrong/restler#events see `complete: function(result, response)`
+            // https://github.com/danwrong/restler#events see `complete: function(result, response)`
+            var err = (result instanceof Error === true) ? result : null;
             if (err) {
                 console.error('Error when saving picture to Nasjonal Turbase', err);
             } else {
@@ -145,48 +87,38 @@ module.exports = function (router) { // TODO: Pass router instead of app as argu
      * Routes
      */
 
-    var JFUM = require('jfum');
-    var jfum = new JFUM();
-
-    var async = require('async');
-
     router.options('/upload/picture', jfum.optionsHandler.bind(jfum));
     router.post('/upload/picture', jfum.postHandler.bind(jfum), function(req, res, next) {
         console.log('POST /upload/picture')
         async.mapSeries(req.jfum.files, function(image, cb) {
             if (image.error) {
                 console.log(image.error);
-                return process.nextTick(async.apply(cb, null, image));
+                return process.nextTick(async.apply(cb, null, {err: image.err, src: image}));
             }
-            console.log(image);
-            s3Client.upload(image.file, function(err, image, exif) {
-                console.log('s3Client cb');
-                console.log(arguments);
+
+            // Resize and upload image to S3
+            s3Client.upload(image.path, {}, function(err, image, meta) {
+                // @TODO handle error
+
+                var geojson;
+
+                // Remove original image
+                image = image.splice(1);
+
+                // Check and get image geoposition
+                if (meta && meta.exif) {
+                    geojson = getImagePosition(meta.exif);
+                }
+
+                // Post image to Nasjonal Turbase
+                saveToNasjonalturbase({img: image, geojson: geojson}, cb);
             });
 
-        }, function(images) {
-            console.log(images);
+        }, function(err, images) {
+            // @TODO handle error
+            res.json({files: images});
         });
     });
-
-    //router.post('/upload/picture', function(req, res) {
-    //  uploader.post(req, res, function (obj) {
-    //    getImagePosition(obj.files[0], function (err, image) {
-    //        uploadImageToS3(image, function (err, images) {
-
-    //            var originalImage = _.findWhere(images, {original: true});
-
-    //            var picture = {img: [], geojson: image.geojson};
-    //            picture.img = _.without(images, originalImage);
-    //            picture.img.push(originalImage);
-
-    //            saveToNasjonalturbase(picture, function (err, picture) {
-    //                res.send(JSON.stringify({files: [picture]})); // Wrapping image in files object and array to match jQuery uploader plugin format
-    //            });
-    //        });
-    //    });
-    //  });
-    //});
 
     return router;
 
