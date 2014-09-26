1b049c600e99d30a59c58abde345f6140e74127f
Håvard Ranum <havard.ranum@bouvet.no>
2014-09-26 13:05:32 +0200

    Added fix checking for empty strings before saving

--- public/scripts/models/ntb.js
+++ public/scripts/models/ntb.js
@@ -9,6 +9,9 @@ define(function (require, exports, module) {
     // Module
     return Backbone.Model.extend({
 
+
+        /* Setters and getters */
+
         setPublished: function(){
             this.set('status', 'Offentlig');
         },
@@ -17,6 +20,63 @@ define(function (require, exports, module) {
             this.set('status', 'Kladd');
         },
 
+
+        /* Process fields */
+
+        // Might not need this anymore
+
+        // cleanTags: function () {
+        //     var tags = this.get('tags');
+
+        //     for (var i = 0; i < tags.length; i++) {
+        //         if (tags[i] === '') {
+        //             tags.splice(i, 1);
+        //         }
+        //     }
+
+        //     this.set('tags', tags);
+        // },
+
+        // Recursive method that will remove all object properties
+        // and array items that are empty strings.
+        removeEmpty: function (input) {
+
+            var output = _.clone(input);
+
+            if (Object.prototype.toString.call(output) === '[object Array]') {
+
+                for (var i = 0; i < output.length; i++) {
+                    if (typeof output[i] === 'object') {
+                        this.removeEmpty(output[i]);
+                    } else if (typeof output[i] === 'string') {
+                        if (output[i] === '') {
+                            output.splice(i, 1);
+                        }
+                    }
+                }
+
+            } else if (Object.prototype.toString.call(output) === '[object Object]') {
+
+                for (var prop in output) {
+                    if (typeof output[prop] === 'object') {
+                        this.removeEmpty(output[prop]);
+                    } else if (typeof output[prop] === 'string') {
+                        if (output[prop] === '') {
+                            delete output[prop];
+                        }
+                    }
+
+                }
+
+            }
+
+            return output;
+
+        },
+
+
+        /* Server interactions */
+
         save: function (attrs, options) {
 
             attrs = attrs || this.toJSON();
@@ -24,7 +84,7 @@ define(function (require, exports, module) {
 
             // If model defines serverAttrs, replace attrs with trimmed version
             if (this.serverAttrs) {
-                attrs = _.pick(attrs, this.serverAttrs);
+                attrs = this.removeEmpty(_.pick(attrs, this.serverAttrs));
             }
 
             // Move attrs to options
