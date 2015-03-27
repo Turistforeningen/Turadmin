8f069fb774d80e6e0d00819b737aae2b203ce8fb
Håvard Ranum <havard.ranum@me.com>
2015-03-27 15:18:07 +0100

    Removed console log

--- public/scripts/collections/pictures.js
+++ public/scripts/collections/pictures.js
@@ -47,7 +47,6 @@ define(function (require, exports, module) {
         },
 
         reIndex: function (picture, newPosition) {
-            console.log('reindexing');
             this.remove(picture, {silent: true}); // NOTE: This causes a bug which removes the picture from POI picture list.
             picture.set('ordinal', newPosition);
             this.each(function (model, index) {
