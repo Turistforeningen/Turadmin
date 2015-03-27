f186d21decf30da3ae3641eab7668b3b56eb6e04
Håvard Ranum <havard.ranum@me.com>
2015-03-27 11:39:25 +0100

    Fixed typo

--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ module.exports = function (app, options) {
             next();
 
         } else {
-            if (req.session && req.session.isAuthneticated === true) {
+            if (req.session && req.session.isAuthenticated === true) {
                 // User has a session and is authenticated
                 next();
             } else {
@@ -38,7 +38,7 @@ module.exports = function (app, options) {
     };
 
     var getLogin = function (req, res, next) {
-        if (req.session && req.session.isAuthneticated === true) {
+        if (req.session && req.session.isAuthenticated === true) {
             res.redirect('/');
         } else {
             var options = {error: req.query.error};
@@ -71,7 +71,7 @@ module.exports = function (app, options) {
             if (err) { console.log(err); return next(err); }
 
             if (result.authenticated === true) {
-                req.session.isAuthneticated = true;
+                req.session.isAuthenticated = true;
                 req.session.authType = 'mitt-nrk';
 
                 req.session.user = {
@@ -92,7 +92,7 @@ module.exports = function (app, options) {
     };
 
     var getLoginDntConnect = function (req, res) {
-        if (req && req.auth && req.auth.isAuthneticated) {
+        if (req && req.auth && req.auth.isAuthenticated) {
             res.redirect('/');
         } else {
             res.redirect(dntConnectClient.signon(BASE_URL + '/login/dnt/verify'));
@@ -115,7 +115,7 @@ module.exports = function (app, options) {
             }
 
             if (data.er_autentisert === true) {
-                req.session.isAuthneticated = true;
+                req.session.isAuthenticated = true;
                 req.session.authType = 'dnt-connect';
 
                 req.session.user = data;
@@ -146,7 +146,7 @@ module.exports = function (app, options) {
                 sentry.captureError(e, { extra: { username: req.body.username }});
 
             } else if (user) {
-                req.session.isAuthneticated = true;
+                req.session.isAuthenticated = true;
                 req.session.authType = 'innholdspartner';
 
                 req.session.user = user;
