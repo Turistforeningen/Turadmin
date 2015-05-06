ac1567899925e509bb5a5bbdfa8c046a8d8c091f
Håvard Ranum <havard.ranum@me.com>
2015-05-06 17:51:27 +0200

    Fetch external groups when loading routes index

--- routes/routes-index.js
+++ routes/routes-index.js
@@ -17,22 +17,37 @@ module.exports = function (app, options) {
         var userGroups = req.userGroups || [];
         var userDefaultRouteFetchQuery = (!!req.signedCookies) ? req.signedCookies['userDefaultRouteFetchQuery_' + req.session.userId] : undefined;
 
+        var areas;
+        var groups;
+
         var onCompleteOmraderRequest = function (data) {
-            var areas = data.documents;
+            areas = data.documents;
+            onCompleteNtbRequest();
+        };
+
+        var onCompleteGrupperRequest = function (data) {
+            groups = data.documents;
+            onCompleteNtbRequest();
+        };
+
+        var onCompleteNtbRequest = underscore.after(2, function () {
             var renderOptions = {
                 title: 'Mine turer',
                 areas: JSON.stringify(areas),
                 userData: JSON.stringify(req.session.user),
                 userGroups: JSON.stringify(userGroups),
+                externalGroups: JSON.stringify(groups),
                 userDefaultRouteFetchQuery: JSON.stringify(userDefaultRouteFetchQuery),
                 authType: req.session.authType,
                 itemType: 'tur'
             };
 
             res.render('routes/index', renderOptions);
-        };
+        });
 
         restProxy.makeApiRequest('/områder/?limit=100&fields=navn,_id&sort=navn&tilbyder=DNT&status=Offentlig', req, undefined, onCompleteOmraderRequest);
+
+        restProxy.makeApiRequest('/grupper/?tags=!&limit=400&fields=navn&sort=navn', req, undefined, onCompleteGrupperRequest);
     };
 
     app.get('/turer', userGroupsFetcher);
